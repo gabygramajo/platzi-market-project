8c5b3099e4481071191f900b7c0774e12d3819da
Gaby <ggabrieldeveloper@gmail.com>
2024-04-03 11:49:30 -0300

    delete: Optional import

--- src/main/java/com/gabydev/platzimarket/web/controller/ProductController.java
+++ src/main/java/com/gabydev/platzimarket/web/controller/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 // @RestController -> indica que será un controlador de una apirest
 // @RequestMapping -> indica en qué ruta se realizará las peticiones, en este caso "/products"
